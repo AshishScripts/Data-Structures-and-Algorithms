7ccca0d71b19b77cae8c88c8bf18638091dfe20a
Ashish <ashishpant545@gmail.com>
2026-03-19 23:45:19 +0530

    Fixed some errors

--- src/basics/CallStackExample.java
+++ src/basics/CallStackExample.java
@@ -2,24 +2,20 @@ package basics;
 
 public class CallStackExample {
 
-    public static void D() {
-        float var1 = 40.5f;
+    public static void D() {    
         System.out.println("In Method D");
     }
 
     public static void C() {
-        double  var2 = 30.5;
         System.out.println("In Method C");
     }
 
     public static void B() {
-        int var3 = 20;
         C(); // Calling C
         System.out.println("In Method B");
     }
 
     public static void A() {
-        int var4 = 10;
         B(); // Calling B
         System.out.println("In Method A");
     }
